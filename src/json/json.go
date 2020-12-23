ba93d02a03f35617b31073c7594ac500b337d3a7
martinporkon <45212111+martinporkon@users.noreply.github.com>
2020-12-23 18:18:36 +0200

    json serialize/marshal and deserialize/unmarshal

---
+++ src/json/json.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Product struct {
+	ProductID      int    `json:"productId`
+	Manufacturer   string `json:"manufacturer`
+	Sku            string `json:"sku`
+	Upc            string `json:"upc`
+	PricePerUnit   string `json:"pricePerUnit`
+	QuantityOnHand int    `json:"quantityOnHand`
+	ProductName    string `json:"productName`
+}
+
+func main() {
+
+	product := &Product{
+		ProductID:      123,
+		Manufacturer:   "Big Box Company",
+		PricePerUnit:   "12.99",
+		Sku:            "4561qHJK",
+		Upc:            "414654444566",
+		QuantityOnHand: 28,
+		ProductName:    "Gizmo",
+	}
+	productJSON, err := json.Marshal(product)
+	// this will return a byte slice or a possible error
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(productJSON)) // prints out the JSON
+	fmt.Println("Hello, playground")
+	product2 := Product{}
+	err2 := json.Unmarshal([]byte(productJSON), &product2)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	fmt.Printf("json was unmarshalled product: %s", product.ProductName)
+}
