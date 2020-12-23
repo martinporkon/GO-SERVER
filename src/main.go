b1d44034d557b852fdc68c4d132f19f0c14b0385
martinporkon <45212111+martinporkon@users.noreply.github.com>
2020-12-23 17:59:28 +0200

    server working

--- src/main.go
+++ src/main.go
@@ -12,6 +12,8 @@ func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(f.Message)) //<< wrties out the message field to the HTTP response using the message writer
 }
 
-func main() {
-	http.Handle("/foo", fooHandler)
+func main() { // see fail on main fail serveri alustamiseks ning tööle panemiseks.
+	http.Handle("/foo", &fooHandler{Message: "foo called"}) // set the message
+	// set the serer to listen and serve
+	http.ListenAndServe(":5000", nil) // nil for the handler and ServeMux. This will tell it to use the default ServeMux
 }
