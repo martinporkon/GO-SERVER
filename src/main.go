ebb2817b8951a4877b4faef2b61a803b1d3aec67
martinporkon <45212111+martinporkon@users.noreply.github.com>
2020-12-23 18:02:01 +0200

    HandleFunc working

--- src/main.go
+++ src/main.go
@@ -15,5 +15,13 @@ func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() { // see fail on main fail serveri alustamiseks ning tööle panemiseks.
 	http.Handle("/foo", &fooHandler{Message: "foo called"}) // set the message
 	// set the serer to listen and serve
-	http.ListenAndServe(":5000", nil) // nil for the handler and ServeMux. This will tell it to use the default ServeMux
+	http.HandleFunc("/bar", barHandler) // bar patterna and the HTTP handleFunc funciton
+	http.ListenAndServe(":5000", nil)   // nil for the handler and ServeMux. This will tell it to use the default ServeMux
+}
+
+// here is the simpler Http call function
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	// accepts a response writer and a pointer to the request
+	w.Write([]byte("bar called"))
 }
