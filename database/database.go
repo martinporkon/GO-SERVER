5c0fae5aa7be53b5a3460feb9d0628cdb2d31d35
martinporkon <45212111+martinporkon@users.noreply.github.com>
2020-12-23 20:27:47 +0200

    database with GET integrated.

--- database/database.go
+++ database/database.go
@@ -16,3 +16,63 @@ func SetupDatabase() {
 }
 
 // we need the Go database driver, as this is not included in the main package.
+
+
+// query the database
+
+func getProductList() ([]Product, error) {
+	results, err := database.DbConn.Query(`SELECT productId,
+	manufacturer,
+	sku,
+	upc,
+	pricePerUnit,
+	quantityOnHand,
+	productName
+	FROM products`)
+	if err != nil {
+		return nil, err
+	}
+	defer results.close()
+	products := make([]Product, 0)
+	for results.Next() {
+		var product Product
+		// all must be in the same order
+		results.Scan(&product.ProductID,
+			 &product.Manufacturer,
+			  &product.Sku,
+			   &product.Upc,
+			&product.PricePerUnit,
+		&product.QuantityOnHand,
+	&product.ProductName)
+	products = append(products, product)
+	}
+	return products, nil
+}
+
+func getProduct(productID int) (*Product, error) {
+	row := database.DbConn.QueryRow(`SELECT productId,
+	manufacturer,
+	sku,
+	upc,
+	pricePerUnit,
+	quantityOnHand,
+	productName
+	FROM products
+	WHERE productId = ?`, productID)// those are query parameters
+	product := &Product{}
+	err := row.Scan(&product.ProductID,
+		&product.Manufacturer,
+		 &product.Sku,
+		  &product.Upc,
+	   		&product.PricePerUnit,
+   		&product.QuantityOnHand,
+		&product.ProductName)
+	if err == sql.ErrNoRows {}
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	return product, nil
+	// w.WriteHeader(InternalServeError)
+	//Works with Postman.
+}
